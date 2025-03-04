37cc03579cabd97de353abd7e6a4340058d148a3
Prolas <93386280+Prolas@users.noreply.github.com>
2025-03-04 11:06:19 +0100

    Start unit testing + new distance metric (#18)
    * Start unit testing + new distance metric
    
    * Tests for substring
    
    * small optimization
    
    * optimize substring
    
    * Multi word
    
    * Bonus
    
    * Inverse condition

--- src/distances/distance_metric.rs
+++ src/distances/distance_metric.rs
@@ -1,5 +1,12 @@
-use std::usize;
+use log::debug;
+use std::{
+    cmp::{max, min},
+    collections::HashMap,
+    ops::Sub,
+    usize,
+};
 
+use polars::prelude::null::MutableNullArray;
 use unicode_segmentation::UnicodeSegmentation;
 
 use crate::word::{GraphemeType, Word};
@@ -251,11 +258,19 @@ impl LvEditNode {
 /// to transform the source word into the target word.
 pub struct LvEditDistanceMetric {
     dp: Vec<LvEditNode>,
+    sub_weight: f32,
+    del_weight: f32,
+    add_weight: f32,
 }
 
 impl LvEditDistanceMetric {
-    pub fn new() -> Self {
-        Self { dp: Vec::new() }
+    pub fn new(sub_weight: f32, del_weight: f32, add_weight: f32) -> Self {
+        Self {
+            dp: Vec::new(),
+            sub_weight,
+            del_weight,
+            add_weight,
+        }
     }
 
     fn idx_at(i: usize, j: usize, len_w2: usize) -> usize {
@@ -360,5 +375,418 @@ impl LvEditDistanceMetric {
 
         let idx = Self::idx_at(len_src, len_trg, len_trg);
         self.get_edit_list(idx)
+    }
+}
+
+fn get_edits_counts(edits: Vec<LvEdit>) -> (f32, f32, f32) {
+    let mut sub_count = 0.;
+    let mut del_count = 0.;
+    let mut add_count = 0.;
+    for edit in edits {
+        match edit {
+            LvEdit::Sub(_index, _grapheme) => {
+                sub_count += 1.;
+            }
+            LvEdit::Del(_index) => {
+                del_count += 1.;
+            }
+            LvEdit::Add(_index, _grapheme) => {
+                add_count += 1.;
+            }
+        }
+    }
+    (sub_count, del_count, add_count)
+}
+
+impl DistanceMetric<Word> for LvEditDistanceMetric {
+    fn dist(&mut self, v1: &Word, v2: &Word) -> f32 {
+        let all_edits = self.compute_edits(v1, v2);
+        let (sub_count, del_count, add_count) = get_edits_counts(all_edits);
+        let edit_count =
+            sub_count * self.sub_weight + del_count * self.del_weight + add_count * self.add_weight;
+        1.0 - edit_count / usize::max(v1.raw.len(), v2.raw.len()) as f32
+    }
+}
+
+pub struct LvSubstringDistanceMetric {
+    dplv: Vec<u8>,
+    dpss: Vec<u8>,
+    weight: f32,
+}
+
+impl LvSubstringDistanceMetric {
+    pub fn new(weight: f32) -> Self {
+        Self {
+            dplv: Vec::new(),
+            dpss: Vec::new(),
+            weight,
+        }
+    }
+
+    fn idx_at(i: usize, j: usize, len_w2: usize) -> usize {
+        i * (len_w2 + 1) + j
+    }
+
+    fn setup_dp(&mut self, len_w1: usize, len_w2: usize) {
+        let size = (len_w1 + 1) * (len_w2 + 1);
+        if size > self.dplv.len() {
+            let additional = size - self.dplv.len();
+            self.dplv.reserve(additional);
+            self.dpss.reserve(additional);
+            // create new elems
+            for _ in 0..additional {
+                self.dpss.push(0);
+                self.dplv.push(0);
+            }
+        }
+
+        // clear all elems
+        self.dplv.fill(0);
+        self.dpss.fill(0);
+    }
+
+    fn compute_edits<'a>(&mut self, mut w1: &'a Word, mut w2: &'a Word) -> (u8, u8) {
+        let mut len_w1 = w1.graphemes.len();
+        let mut len_w2 = w2.graphemes.len();
+
+        // w1 must be the largest word
+        if len_w1 < len_w2 {
+            (len_w1, len_w2) = (len_w2, len_w1);
+            (w1, w2) = (w2, w1);
+        }
+
+        self.setup_dp(len_w1, len_w2);
+
+        for i in 1..(len_w1 + 1) {
+            let idx = Self::idx_at(i, 0, len_w2);
+            self.dplv[idx] = i as u8;
+        }
+
+        // set a boundary after the diagonal
+        for j in 1..(len_w2 + 1) {
+            let idx = Self::idx_at(j - 1, j, len_w2);
+            self.dplv[idx] = 255;
+        }
+
+        let mut longest = 0;
+
+        for i in 1..(len_w1 + 1) {
+            let g1 = w1.graphemes[i - 1];
+
+            let to = usize::min(i + 1, len_w2 + 1);
+            for j in 1..(to) {
+                let g2 = w2.graphemes[j - 1];
+
+                let idx_cur = Self::idx_at(i, j, len_w2);
+                if g1 == g2 {
+                    let idx_prev = Self::idx_at(i - 1, j - 1, len_w2);
+                    self.dplv[idx_cur] = self.dplv[idx_prev];
+                    self.dpss[idx_cur] = self.dpss[idx_prev] + 1;
+                    longest = max(longest, self.dpss[idx_cur]);
+                    continue;
+                }
+
+                let idx_sub = Self::idx_at(i - 1, j - 1, len_w2);
+                let idx_del = Self::idx_at(i - 1, j, len_w2);
+                let idx_add = Self::idx_at(i, j - 1, len_w2);
+
+                let len_sub = self.dplv[idx_sub];
+                let len_del = self.dplv[idx_del];
+                let len_add = self.dplv[idx_add];
+
+                if len_sub < len_del && len_sub < len_add {
+                    self.dplv[idx_cur] = len_sub + 1;
+                } else if len_del < len_add {
+                    self.dplv[idx_cur] = len_del + 1;
+                } else {
+                    self.dplv[idx_cur] = len_add + 1;
+                }
+            }
+        }
+
+        let idx = Self::idx_at(len_w1, len_w2, len_w2);
+        (self.dplv[idx], longest)
+    }
+}
+
+impl DistanceMetric<Word> for LvSubstringDistanceMetric {
+    fn dist(&mut self, v1: &Word, v2: &Word) -> f32 {
+        let (edits, longest_substring) = self.compute_edits(v1, v2);
+        let bonus = longest_substring as f32 * self.weight;
+        let edits = f32::max(edits as f32 - bonus, 0.0);
+        1.0 - edits / usize::max(v1.raw.len(), v2.raw.len()) as f32
+    }
+}
+
+pub struct LvMultiWordDistanceMetric {
+    lvopti: LvOptiDistanceMetric,
+    separator: GraphemeType,
+}
+
+impl LvMultiWordDistanceMetric {
+    pub fn new(separator: GraphemeType) -> Self {
+        Self {
+            lvopti: LvOptiDistanceMetric::new(),
+            separator,
+        }
+    }
+
+    fn extract_multiple_words(w: &Word, separator: GraphemeType) -> Vec<Vec<GraphemeType>> {
+        let mut ret: Vec<Vec<GraphemeType>> = Vec::new();
+        let mut temp: Vec<GraphemeType> = Vec::new();
+
+        for g in &w.graphemes {
+            if *g == separator {
+                if !temp.is_empty() {
+                    ret.push(temp);
+                }
+                temp = Vec::new();
+            } else {
+                temp.push(*g);
+            }
+        }
+        if !temp.is_empty() {
+            ret.push(temp);
+        }
+        ret
+    }
+
+    fn compute_edits(&mut self, w1: &Word, w2: &Word) -> u8 {
+        let multi_w1 = Self::extract_multiple_words(w1, self.separator);
+        let multi_w2 = Self::extract_multiple_words(w2, self.separator);
+
+        let mut total = 0;
+        let mut perfect_matches = 0;
+
+        let len1 = multi_w1.len();
+        let len2 = multi_w2.len();
+
+        if !(len1 == len2 && len1 > 1) {
+            return self.lvopti.compute_edits(w1, w2);
+        }
+        for i in 0..len1 {
+            let w1 = &Word::from_graphemes(multi_w1[i].clone());
+            let w2 = &Word::from_graphemes(multi_w2[i].clone());
+
+            let edits = self.lvopti.compute_edits(w1, w2);
+            if edits == 0 {
+                perfect_matches += 1;
+            }
+            total += edits;
+        }
+
+        let factor = 1.0 - (perfect_matches as f32) / (len1 as f32);
+        return (total as f32 * factor) as u8;
+    }
+}
+
+impl DistanceMetric<Word> for LvMultiWordDistanceMetric {
+    fn dist(&mut self, v1: &Word, v2: &Word) -> f32 {
+        let edits = self.compute_edits(v1, v2);
+        1.0 - edits as f32 / usize::max(v1.raw.len(), v2.raw.len()) as f32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Helper function to create a Word instance from a string
+    fn create_word(s: &str) -> Word {
+        Word::new(s.to_string())
+    }
+
+    #[test]
+    fn test_lv_distance_metric() {
+        let mut metric = LvDistanceMetric::new();
+
+        // Test identical words
+        let w1 = create_word("hello");
+        let w2 = create_word("hello");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 0);
+        // Test one empty word
+        let w2 = create_word("");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+
+        // Test single character difference
+        let w2 = create_word("hallo");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1);
+
+        // Test different lengths
+        let w2 = create_word("helloworld");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5); // More edits, distance should be lower
+
+        // Test partial overlap
+        let w1 = create_word("bernart");
+        let w2 = create_word("jeanbernard");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5); // Partial overlap, distance should be high (close to 1)
+
+        // Test case sensitivity
+        let w1 = create_word("Hello");
+        let w2 = create_word("hello");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1); // Case-sensitive comparison
+
+        let w1 = create_word("Bernard");
+        let w2 = create_word("bBeernard");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 2);
+    }
+
+    #[test]
+    fn test_lv_opti_distance_metric() {
+        let mut metric = LvOptiDistanceMetric::new();
+
+        // Test identical words
+        let w1 = create_word("hello");
+        let w2 = create_word("hello");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 0);
+        // Test one empty word
+        let w2 = create_word("");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+
+        // Test single character difference
+        let w2 = create_word("hallo");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1);
+
+        // Test different lengths
+        let w2 = create_word("helloworld");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+
+        // Test partial overlap
+        let w1 = create_word("bernart");
+        let w2 = create_word("jeanbernard");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+
+        // Test case sensitivity
+        let w1 = create_word("Hello");
+        let w2 = create_word("hello");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1); // Case-sensitive comparison
+
+        let w1 = create_word("Bernard");
+        let w2 = create_word("bBeernard");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 2);
+    }
+
+    #[test]
+    fn test_lv_substring_metric() {
+        let mut metric = LvSubstringDistanceMetric::new(1.0);
+
+        // Test identical words
+        let w1 = create_word("hello");
+        let w2 = create_word("hello");
+        let (distance, longest) = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 0);
+        assert_eq!(longest, 5);
+
+        // Test one empty word
+        let w2 = create_word("");
+        let (distance, longest) = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+        assert_eq!(longest, 0);
+
+        // Test single character difference
+        let w2 = create_word("hallo");
+        let (distance, longest) = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1);
+        assert_eq!(longest, 3);
+
+        // Test different lengths
+        let w2 = create_word("helloworld");
+        let (distance, longest) = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+        assert_eq!(longest, 5);
+
+        // Test partial overlap
+        let w1 = create_word("bernart");
+        let w2 = create_word("jeanbernard");
+        let (distance, longest) = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+        assert_eq!(longest, 6);
+
+        // Test case sensitivity
+        let w1 = create_word("Hello");
+        let w2 = create_word("hello");
+        let (distance, longest) = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1); // Case-sensitive comparison
+        assert_eq!(longest, 4);
+
+        let w1 = create_word("Bernard");
+        let w2 = create_word("bBeernard");
+        let (distance, longest) = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 2);
+        assert_eq!(longest, 6);
+    }
+
+    #[test]
+    fn test_lv_multi_word_metric() {
+        let separator = Word::string_to_grapheme(" ");
+        let mut metric = LvMultiWordDistanceMetric::new(separator);
+        // Test identical words
+        let w1 = create_word("hello");
+        let w2 = create_word("hello");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 0);
+
+        // Test one empty word
+        let w2 = create_word("");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+
+        // Test single character difference
+        let w2 = create_word("hallo");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1);
+
+        // Test different lengths
+        let w2 = create_word("helloworld");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+
+        // Test partial overlap
+        let w1 = create_word("bernart");
+        let w2 = create_word("jeanbernard");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 5);
+
+        // Test case sensitivity
+        let w1 = create_word("Hello");
+        let w2 = create_word("hello");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 1); // Case-sensitive comparison
+
+        let w1 = create_word("Bernard");
+        let w2 = create_word("bBeernard");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 2);
+
+        let w1 = create_word("inspecteur des impots");
+        let w2 = create_word("insp des impots");
+        let distance = metric.compute_edits(&w1, &w2);
+        // 2 perfect matches => 6 * (1 - 2/3) == 1 (not 2)
+        assert_eq!(distance, 1);
+
+        let w1 = create_word("inspecteur impots");
+        let w2 = create_word("insp des impots");
+        let distance = metric.compute_edits(&w1, &w2);
+        // classic distance
+        assert_eq!(distance, 5);
+
+        let w1 = create_word("inspecteur des impots");
+        let w2 = create_word("inspe ds impts");
+        let distance = metric.compute_edits(&w1, &w2);
+        assert_eq!(distance, 7);
     }
 }
